9eb16f56923288aeeade65589444c3e43549aebf
Caua Fernndo <cauafernadodesouza076@gmail.com>
2024-06-21 18:57:48 -0300

    Adicionando um novo projeto

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,10 +1,20 @@
-const fund = document.querySelector(".fundo");
+const fund1 = document.querySelector("#fundo-1");
+const calcjuros = document.querySelector(".calc-juros");
+const fechar1 = document.querySelector("#fechar-1");
+const fund2 = document.querySelector("#fundo-2");
 const telalog = document.querySelector(".tela-login");
-const fechar = document.querySelector(".fechar");
-
-telalog.addEventListener('click', ()=> { fund.classList.add('abrir-jane')
+const fechar2 = document.querySelector("#fechar-2");
+calcjuros.addEventListener('click', ()=> { 
+    fund1.classList.add('abrir-jane')
 });
-fechar.addEventListener('click', ()=>  {
-    fund.classList.remove('abrir-jane')
+fechar1.addEventListener('click', ()=>  {
+    fund1.classList.remove('abrir-jane')
+}
+);
+telalog.addEventListener('click', ()=> { 
+    fund2.classList.add('abrir-jane')
+});
+fechar2.addEventListener('click', ()=>  {
+    fund2.classList.remove('abrir-jane')
 }
 );
